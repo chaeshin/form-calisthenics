0b2923bdf25d0a630b87a6d415b6ef7616925de4
Nicholas Matsumoto <nk.matsumoto@gmail.com>
2024-08-19 17:16:53 +0900

    Created initial associations and validations

--- app/models/exercise_set.rb
+++ app/models/exercise_set.rb
@@ -1,4 +1,8 @@
 class ExerciseSet < ApplicationRecord
   belongs_to :workout_session
   belongs_to :exercise
+
+  validates :reps, :duration, numericality: { greater_than_or_equal_to: 0 }
+  validates :workout_session, presence: true
+  validates :exercise, presence: true
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,9 @@
 class Workout < ApplicationRecord
   belongs_to :user
+  has_many :exercise_assignments, dependent: :destroy
+  has_many :exercises, through: :exercise_assignments
+  has_many :exercise_sets, through: :workout_sessions
+
+  validates :name, presence: true
+  validates :category, presence: true
 end
