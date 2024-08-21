8f0538de3eab1e1ae32aa26dbcaa30caab42bb33
Ryo Imaoka <Stuntpad@gmail.com>
2024-08-21 12:33:43 +0900

    added pages home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-
+    if params[:query].present?
+      @workouts = Workout.search_by(params[:query])
+    else
+      @workouts = Workout.all
+    end
   end
 
   def dashboard
