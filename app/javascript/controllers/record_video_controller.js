bb11846c2590988a6eb260c73f0bc7fa3daf6fec
Chaewan Shin <chaewanshin@gmail.com>
2024-08-22 22:40:36 +0900

    link to video page updated

--- app/javascript/controllers/record_video_controller.js
+++ app/javascript/controllers/record_video_controller.js
@@ -2,9 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="record-video"
 export default class extends Controller {
-  static targets = ["start", "stop", "live", "form", "collapse", "video"]
+  static targets = ["start", "stop", "live", "form", "video"]
   connect() {
-    console.log(this.startTarget, this.stopTarget, this.liveTarget, this.collapseTarget)
+    console.log(this.startTarget, this.stopTarget, this.liveTarget)
   }
   record() {
     navigator.mediaDevices.getUserMedia({
@@ -56,7 +56,7 @@ export default class extends Controller {
   replace(data) {
     this.videoTarget.innerHTML = data;
     this.videoTarget.classList.remove("d-none")
-    this.collapseTarget.classList.add("d-none")
+    // this.collapseTarget.classList.add("d-none")
   }
 
   uploadToCloudinary(video) {
