866aed7c6bd1825c4f1200621c2d81e9f8a5f3c5
Chaewan Shin <chaewanshin@gmail.com>
2024-08-23 13:34:43 +0900

    video replaced

--- app/controllers/workout_sessions_controller.rb
+++ app/controllers/workout_sessions_controller.rb
@@ -12,6 +12,9 @@ class WorkoutSessionsController < ApplicationController
   def show
     @workout_session = WorkoutSession.find(params[:id])
     @exercises = @workout_session.exercises
+    # need exercise_sets for the exercise and the workout_session
+    @exercise_sets = @exercises.
+    # create instance(@) for the exercise_sets for the workout_session
   end
 
   def create
