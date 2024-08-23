5bc4a01baf577bcaad99159030bb00b3cde4c0cf
Nicholas Matsumoto <nk.matsumoto@gmail.com>
2024-08-23 14:03:26 +0900

    pushing heroku

--- app/controllers/workout_sessions_controller.rb
+++ app/controllers/workout_sessions_controller.rb
@@ -14,7 +14,7 @@ class WorkoutSessionsController < ApplicationController
     @workout_session = WorkoutSession.find(params[:id])
     @exercises = @workout_session.exercises
     # need exercise_sets for the exercise and the workout_session
-    @exercise_sets = @exercises.
+    # @exercise_sets = @exercises
     # create instance(@) for the exercise_sets for the workout_session
   end
 
