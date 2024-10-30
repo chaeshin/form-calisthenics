19e1c587b63c0a51b064deeb60a96a336ab58efd
Chaewan Shin <chaewanshin@gmail.com>
2024-10-30 19:14:21 +0900

    PWA setting updated

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,11 @@ import "@popperjs/core"
 import "bootstrap"
 import "chartkick"
 import "Chart.bundle"
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => console.log('Service Worker registered:', registration))
+      .catch(error => console.log('Service Worker registration failed:', error));
+  });
+}
