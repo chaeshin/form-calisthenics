bc0b0520fe79858d3e39f9e52f968a4de5b59c2b
Chaewan Shin <chaewanshin@gmail.com>
2024-11-08 14:39:00 +0900

    delete dependent added to workoutsession model

--- app/models/workout_session.rb
+++ app/models/workout_session.rb
@@ -1,9 +1,9 @@
 class WorkoutSession < ApplicationRecord
   belongs_to :user
   belongs_to :workout
-  has_many :exercise_sets
-  has_many :exercise_assignments, through: :workout
-  has_many :exercises, through: :exercise_assignments
+  has_many :exercise_sets, dependent: :delete_all
+  has_many :exercise_assignments, through: :workout, dependent: :delete_all
+  has_many :exercises, through: :exercise_assignments, dependent: :delete_all
 
   def sets_done(exercise)
     self.exercise_sets.select { |ex| ex.exercise == exercise }
