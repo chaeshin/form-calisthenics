a216622da5c3b11ecde415b58cb388bdcb98fc8b
Chaewan Shin <chaewanshin@gmail.com>
2024-11-19 22:28:37 +0900

    updated workout session

--- app/models/workout_session.rb
+++ app/models/workout_session.rb
@@ -2,8 +2,8 @@ class WorkoutSession < ApplicationRecord
   belongs_to :user
   belongs_to :workout
   has_many :exercise_sets
-  has_many :exercises, through: :exercise_assignments
   has_many :exercise_assignments, through: :workout
+  has_many :exercises, through: :exercise_assignments
   def sets_done(exercise)
     self.exercise_sets.select { |ex| ex.exercise == exercise }
   end
